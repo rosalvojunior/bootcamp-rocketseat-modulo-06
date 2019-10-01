457ec67d0e474b8457e70e9a5f73cbd912de3313
Rosalvo Júnior <rosalvo@enesolucoes.com.br>
2019-10-01 16:42:21 -0300

    Estilizando formulário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = { title: 'Usuários' };
